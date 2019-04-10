739c48c33466e9472f1456f2f50c555f4228ab0d
Leonardo Arruda Mesquita - LMS <leonardo.mesquita@iteris.local>
2019-04-10 14:04:49 -0300

    Desafio js

---
+++ JogoDaVelha/JogoDaVelha.js
@@ -0,0 +1,13 @@
+function Main() {
+
+//     //Relaciona o elemento canvas com o js
+//     var tela = document.querySelector("#myCanvas");
+
+//     //O contexto possui parâmetros para desenhar
+//     var pincel = tela.getContext("2d");
+
+//     pincel.fillStyle = "orange";
+//     pincel.fillRect(0, 0, 200, 200);
+}
+
+document.addEventListener("DOMContentLoaded", Main);
