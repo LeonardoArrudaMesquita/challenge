26f2f738c3e08fc99e486f3d195c98061864ef8b
Leonardo Arruda Mesquita - LMS <leonardo.mesquita@iteris.local>
2019-04-11 16:01:08 -0300

    Mudanças no layout e na lógica

--- JogoDaVelha/JogoDaVelha.js
+++ JogoDaVelha/JogoDaVelha.js
@@ -1,22 +1,103 @@
+// --- REGRAS ---
+// X = 1
+// O = -1
 
-move = 9; 
+//Número da jogada
+move = 9;
 
-//var evento = event.target.getElementById;
-//debugger;
+//Array que representa as casas do tabuleiro
+arrField = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+var point;
 
-function play(){
+function play(idElementClicked) {
 
-    if( move % 2 == 0)
-    {        
-        this.getElementById(this).innerHTML = '<img src="img/circle.png">';
-        document.getElementById(this).innerHTML = '<img src="img/circle.png">';
+    switch (idElementClicked) {
+        case "square1":
+            arrField[0] = setElement(idElementClicked);
+            break;
+        case "square2":
+            arrField[1] = setElement(idElementClicked);
+            break;
+        case "square3":
+            arrField[2] = setElement(idElementClicked);
+            break;
+        case "square4":
+            arrField[3] = setElement(idElementClicked);
+            break;
+        case "square5":
+            arrField[4] = setElement(idElementClicked);
+            break;
+        case "square6":
+            arrField[5] = setElement(idElementClicked);
+            break;
+        case "square7":
+            arrField[6] = setElement(idElementClicked);
+            break;
+        case "square8":
+            arrField[7] = setElement(idElementClicked);
+            break;
+        case "square9":
+            arrField[8] = setElement(idElementClicked);
+            break;
     }
-    else
-    {
-        this.getElementById(this).innerHTML = '<img src="img/x.png">';
-    }
-    move--;
-
+    rule();
 }
 
-// btn.addEventListener("click", function(){titulo.innerText= txt.value;});}
+function setElement(idElementClicked) {
+
+    if (move % 2 == 0 && move != 0) {
+        document.getElementById(idElementClicked).innerHTML = '<p>X</p>';
+        move--;
+        return -1;
+    }
+    else {
+        document.getElementById(idElementClicked).innerHTML = '<p>O</p>';
+        move--;        
+        return 1;
+    }
+}
+
+function rule() {
+
+    for (let i = 0; i < arrField.length; i+=3) {
+        for (let j = 0; j < 3; j++) {
+            
+            if(arrField[i + j] == 1)
+            {
+                point++;                
+            }
+        }        
+    }    
+}
+
+// function rule() {
+
+//     for (let i = 0; index < arrField.lenght; index++) {
+//         for (let j = 0; index < array.length; index++) {
+            
+            
+//         }        
+//     }    
+// }
+
+
+// if (arrField[0] == 1 && arrField[1] == 1 && arrField[2] == 1 ) {
+    //     alert("O X ganhou !");
+    // }
+    // else if(arrField[0] == 1 && arrField[1] == 1 && arrField[2] == 1 ) {
+    //     alert("A Bolinha ganhou !");
+    // }
+
+    // if (arrField[3] == 1 && arrField[4] == 1 && arrField[5] == 1 ) {
+    //     alert("O X ganhou !");
+    // }
+    // else if(arrField[3] == 1 && arrField[4] == 1 && arrField[5] == 1 ) {
+    //     alert("A Bolinha ganhou !");
+    // }
+
+    // if (arrField[0] == 1 && arrField[1] == 1 && arrField[2] == 1 ) {
+    //     alert("O X ganhou !");
+    // }
+    // else if(arrField[0] == 1 && arrField[1] == 1 && arrField[2] == 1 ) {
+    //     alert("A Bolinha ganhou !");
+    // }
